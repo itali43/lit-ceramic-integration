6b24c8c26082a3cd85633f321ed4918cbee49703
Elliott <itali43@gmail.com>
2021-11-30 15:06:38 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+# test code to start module
+
+module.exports = function tiny(string) {
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  return string.replace(/\s/g, "");
+};
+

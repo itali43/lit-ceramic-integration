cebecb6ebd300e94bd0538237092ede58334e50e
Elliott <itali43@gmail.com>
2021-12-20 02:07:19 -0600

    tsig edit

--- lit.ts
+++ lit.ts
@@ -22,8 +22,8 @@ function blobToBase64(blob: Blob) {
   return new Promise((resolve, _) => {
     const reader = new FileReader();
     reader.onloadend = () =>
-      // @ts-ignore
       resolve(
+        // @ts-ignore
         reader.result.replace("data:application/octet-stream;base64,", "")
       );
     reader.readAsDataURL(blob);
